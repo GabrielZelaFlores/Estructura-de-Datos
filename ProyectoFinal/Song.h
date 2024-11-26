4e8157887fe307b50e352202dfd025c0d141411c
JuanHuamaniVasquez <jhuamaniv@ulasalle.edu.pe>
2024-11-26 15:11:07 -0500

    Create Song.h

---
+++ ProyectoFinal/Song.h
@@ -0,0 +1,31 @@
+#ifndef SONG_H
+#define SONG_H
+
+#include <string>
+
+class Song {
+private:
+    std::string trackId;
+    std::string artistName;
+    std::string trackName;
+    std::string genre;
+    int year;
+    int popularity;
+    int duration;
+
+public:
+    Song(const std::string& id, const std::string& artist, const std::string& track,
+         const std::string& genre, int year, int popularity, int duration)
+        : trackId(id), artistName(artist), trackName(track), genre(genre),
+          year(year), popularity(popularity), duration(duration) {}
+
+    const std::string& getTrackId() const { return trackId; }
+    const std::string& getArtistName() const { return artistName; }
+    const std::string& getTrackName() const { return trackName; }
+    const std::string& getGenre() const { return genre; }
+    int getYear() const { return year; }
+    int getPopularity() const { return popularity; }
+    int getDuration() const { return duration; }
+};
+
+#endif
